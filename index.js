02b241e91fe17c3a2c420a449773a435a73984d7
嶋川　知宜 <s2002t9021@icloud.com>
2022-06-08 09:51:50 +0900

    6/8

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 for(let n = 1; n <= 16; n = n + 1){
-    const piece01 = document.querySelector('.pos-' + n);
+    const piece = document.querySelector('.pos-' + n);
     piece.style.order = parseInt(Math.random() * 16) + 1;
 }
